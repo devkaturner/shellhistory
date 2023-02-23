7d1b98332d6b6f6dfc3a93cfda322c6a5905e093
Kristen Turner <124066697+devkaturner@users.noreply.github.com>
2023-02-23 19:57:57 +0000

    we added a or statement so that it will recognize lowercase letters in the answer for "Brian Fox"

--- shellhistory.sh
+++ shellhistory.sh
@@ -6,7 +6,7 @@ read input
 if [ "$input" == "yes" ]; then
   echo "You must be in cohort 6! What's their name?"
   read name
-  if [ "$name" == "Brian Fox" ]; then
+  if [ "$name" == "Brian Fox" ] || [ "brian fox" ]; then
     echo "Yes! Brian Jhan Fox (born 1959) is an American computer programmer and free software advocate. He is the original author of the GNU Bash shell, which he announced as a beta in June 1989.[1] He continued as the primary maintainer of bash until at least early 1993."
   else
     echo "Sorry, that's not the correct name."
